fd351a7923206da9ab5c49aec8d03314923c1225
Dddpwpp <147535933+Dddpwpp@users.noreply.github.com>
2024-04-15 14:28:00 -0400

    Create AnuralFS.sh

---
+++ AnuralFS.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Run this script as root (sudo su)"
+    exit
+fi
+
+echo "+##############################################+"
+echo "# Welcome To PollenFS!                         #"
+echo "# The User Policy Editor                       #"
+echo "# -------------------------------------------- #"
+echo "# Developers:                                  #"
+echo "# - Xotic                                      #"
+echo "# - OlyB                                       #"
+echo "# - Rafflesia                                  #"
+echo "# - Scaratek                                   #"
+echo "# - r58Playz                                   #"
+echo "+##############################################+"
+echo "May Ultrablue Rest in Peace, o7"
+
+sleep 1
+
+mkdir -p /etc/opt/chrome/policies/managed
+printf '
+{
+    "URLBlocklist": [], 
+    "SystemFeaturesDisableList": [], 
+    "EditBookmarksEnabled": true, 
+    "ChromeOsMultiProfileUserBehavior": "unrestricted", 
+    "DeveloperToolsAvailability": 1, 
+    "DefaultPopupsSetting": 1, 
+    "AllowDeletingBrowserHistory": true, 
+    "AllowDinosaurEasterEgg": true, 
+    "IncognitoModeAvailability": 0, 
+    "AllowScreenLock": true, 
+    "ExtensionAllowedTypes": null, 
+    "ExtensionInstallAllowlist": null, 
+    "ExtensionInstallBlocklist": null, 
+    "ExtensionInstallForcelist": null, 
+    "ExtensionSettings": null, 
+    "LacrosAvailability": "user_choice", 
+    "WallpaperImage": null
+}
+' > /etc/opt/chrome/policies/managed/pollen.json
+
+echo ""
+echo "PollenFS has been successfully applied!"
